779dd2d20cec836e4960aecb7a9d159184e9d4d9
Kenta Sato <karupa@cpan.org>
2011-09-03 17:02:41 +0900

    とりあえずのものだけ出来た。

--- lib/Class/VirtualAccessor.pm
+++ lib/Class/VirtualAccessor.pm
@@ -3,16 +3,65 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Carp ();
+use Variable::Magic ();
+
+sub import {
+    my $class  = shift;
+    my $args   = (@_ == 1) ? $_[0] : +{ @_ };
+    return if $args->{disable};
+
+    my $caller = caller;
+    my $bless = sub {
+        croak('This module support HashRef only.') if(ref $_[0] ne 'HASH');
+        my $obj = CORE::bless($_[0] => $_[1]);
+
+        if (exists $args->{ro}) {
+            my $wiz = Variable::Magic::wizard(set => sub { Carp::croak('This variable is read only.') });
+            foreach my $name ( @{$args->{ro}} ) {
+                Variable::Magic::cast($obj->{$name}, $wiz);
+            }
+        }
+        if (exists $args->{wo}) {
+            my $wiz = Variable::Magic::wizard(get => sub { Carp::croak('This variable is write only.') });
+            foreach my $name ( @{$args->{wo}} ) {
+                Variable::Magic::cast($obj->{$name}, $wiz);
+            }
+        }
+        if (exists $args->{rw}) {
+            foreach my $name ( @{$args->{rw}} ) {
+                $obj->{$name} = undef unless exists $obj->{$name}; # touch
+            }
+        }
+
+        my $wrap_wiz = Variable::Magic::wizard(
+            fetch => sub { Carp::croak(qq{This variable "$_[2]" is not defined.}) unless exists $_[0]->{$_[2]} },
+            store => sub { Carp::croak(qq{This variable "$_[2]" is not defined.}) unless exists $_[0]->{$_[2]} },
+        );
+        Variable::Magic::cast(%$obj, $wrap_wiz);
+
+        return $obj;
+    };
+
+    {
+        no strict 'refs';
+        *{"${caller}::bless"} = $bless;
+    }
+}
+
 1;
 __END__
 
 =head1 NAME
 
-Class::VirtualAccessor -
+Class::VirtualAccessor - 
 
 =head1 SYNOPSIS
 
-  use Class::VirtualAccessor qw/hoge fuga hogefuga/;
+  use Class::VirtualAccessor (
+    rw => [qw/hoge fuga/],
+    ro => [qw/foo/],
+  );
 
   sub new {
       my $class = shift;
---
+++ t/01_basic.t
@@ -0,0 +1,35 @@
+package Foo;
+use Class::VirtualAccessor (
+    ro => [qw/hoge/],
+    wo => [qw/foobar/],
+    rw => [qw/fuga/],
+);
+
+sub new {
+    my $class = shift;
+    my $args  = (@_ == 1) ? $_[0] : +{ @_ };
+
+    bless(+{ %$args } => $class);
+}
+
+package main;
+use strict;
+use warnings;
+use Test::More tests => 11;
+use Test::Exception;
+
+my $obj;
+lives_ok { $obj = Foo->new(hoge => 'fuga') } 'bless ok';
+lives_ok { $obj->{fuga} = 'hoge' } 'rw variable write ok';
+lives_ok { $obj->{foobar} = 'foobar' } 'wo variable write ok';
+lives_ok {
+    is $obj->{hoge}, 'fuga', 'read ok';
+} 'ro variable write ok';
+lives_ok { $obj->{fuga} } 'rw variable write ok';
+lives_ok {
+    $obj->{fuga} = $obj->{hoge};
+    is $obj->{fuga}, 'fuga', 'read ok';
+} 'rw variable write ok';
+dies_ok { $obj->{fugo} } 'no defined variable';
+dies_ok { $obj->{hoge} = $obj->{fuga} } 'ro variable write fail ok';
+dies_ok { $obj->{foobar} } 'wo variable read fail ok';
